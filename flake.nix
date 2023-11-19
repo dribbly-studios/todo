1afd38d489001ceecb793d9e6d64640092af80d5
Alex Demetriou <alex@demetriou.dev>
2023-11-19 23:30:53 +0000

    chore: restructure flake

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
       pkgs = import nixpkgs { system = "x86_64-linux"; };
       cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
       system = "x86_64-linux";
+
       eachSystem = f: nixpkgs.lib.genAttrs [ system ] (system: f nixpkgs.legacyPackages.${system});
 
       treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs {
@@ -16,10 +17,31 @@
       });
     in
     {
-      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
-        inherit (cargoToml.package) name version;
-        src = ./.;
-        cargoLock.lockFile = ./Cargo.lock;
+      builds = {
+        api = pkgs.rustPlatform.buildRustPackage {
+          inherit (cargoToml.package) name version;
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+
+        app = null;
+      };
+
+      images = {
+        api = pkgs.dockerTools.buildLayeredImage {
+          name = cargoToml.package.name;
+          tag = cargoToml.package.version;
+
+          contents = [ packages.${system}.todoApi ];
+          config = {
+            Cmd = [ "${todoApi}/bin/${todoApi.pname}" ];
+            ExposedPorts = {
+              "3000/tcp" = { };
+            };
+          };
+        };
+
+        app = null;
       };
 
       formatter = eachSystem (pkgs: treefmtEval.${system}.config.build.wrapper);
@@ -34,6 +56,11 @@
         '';
         buildInputs = [ pkgs.libiconv pkgs.rustc pkgs.cargo pkgs.cargo-watch pkgs.just pkgs.rust-analyzer pkgs.rustPlatform.rustcSrc ];
         RUST_BACKTRACE = 1;
+      };
+
+      packages.${system} = rec {
+        inherit (self.builds) api app;
+        default = api;
       };
     };
 }
