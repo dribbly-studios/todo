57fc56ffe764de3992686ae87e8a30b765574b5f
Alex Demetriou <alex@demetriou.dev>
2023-11-20 00:13:59 +0000

    chore: docker output in flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.treefmt-nix.url = "github:numtide/treefmt-nix";
 
-  outputs = inputs: with inputs;
+  outputs = { self, nixpkgs, treefmt-nix }:
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
       cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
@@ -15,52 +15,40 @@
         programs.rustfmt.enable = true;
         programs.nixpkgs-fmt.enable = true;
       });
-    in
-    {
-      builds = {
-        api = pkgs.rustPlatform.buildRustPackage {
-          inherit (cargoToml.package) name version;
-          src = ./.;
-          cargoLock.lockFile = ./Cargo.lock;
-        };
 
-        app = null;
+      default = pkgs.rustPlatform.buildRustPackage {
+        inherit (cargoToml.package) name version;
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
       };
 
-      images = {
-        api = pkgs.dockerTools.buildLayeredImage {
-          name = cargoToml.package.name;
-          tag = cargoToml.package.version;
-
-          contents = [ packages.${system}.todoApi ];
-          config = {
-            Cmd = [ "${todoApi}/bin/${todoApi.pname}" ];
-            ExposedPorts = {
-              "3000/tcp" = { };
-            };
+      image = pkgs.dockerTools.buildLayeredImage {
+        inherit (cargoToml.package) name;
+        tag = cargoToml.package.version;
+        contents = [ default ];
+        config = {
+          Cmd = [ "${default}/bin/${default.name}" ];
+          ExposedPorts = {
+            "3000/tcp" = { };
           };
         };
-
-        app = null;
       };
-
+    in
+    {
       formatter = eachSystem (pkgs: treefmtEval.${system}.config.build.wrapper);
       checks = eachSystem (pkgs: {
         formatting = treefmtEval.${system}.config.build.check self;
       });
 
       devShells.${system}.default = pkgs.mkShell {
-        shellHook = ''
-          export RUST_SRC_PATH=${pkgs.rustPlatform.rustLibSrc}
-          just
-        '';
         buildInputs = [ pkgs.libiconv pkgs.rustc pkgs.cargo pkgs.cargo-watch pkgs.just pkgs.rust-analyzer pkgs.rustPlatform.rustcSrc ];
+
         RUST_BACKTRACE = 1;
+        RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
       };
 
-      packages.${system} = rec {
-        inherit (self.builds) api app;
-        default = api;
+      packages.${system} = {
+        inherit default image;
       };
     };
 }
